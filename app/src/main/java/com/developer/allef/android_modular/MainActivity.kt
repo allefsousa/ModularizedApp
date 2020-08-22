101a895fd4e307869007c2279e2c305d52a2eedb
Allef Sousa <allefsousa_1@hotmail.com>
2020-08-22 19:52:00 -0300

    Navegação por modulos tradicional

--- app/src/main/java/com/developer/allef/android_modular/MainActivity.kt
+++ app/src/main/java/com/developer/allef/android_modular/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.developer.allef.android_modular
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.developer.allef.feature1.Feature1Activity
+import com.developer.allef.feature2.Feature2Activity
+import com.developer.allef.feature3.Feature3Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_feature1.setOnClickListener {
+            val intent = Intent()
+                    .setClassName(this, Feature1Activity::class.java.name)
+            startActivity(intent)
+        }
+
+        btn_feature2.setOnClickListener {
+            val intent = Intent()
+                    .setClassName(this, Feature2Activity::class.java.name)
+            startActivity(intent)
+        }
+        btn_feature3.setOnClickListener {
+            val nextFrag = Feature3Fragment()
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, nextFrag, "findThisFragment")
+                    .addToBackStack(null)
+                    .commit()
+        }
+
+
     }
 }
---
+++ features/feature1/src/main/java/com/developer/allef/feature1/Feature1Activity.kt
@@ -0,0 +1,11 @@
+package com.developer.allef.feature1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Feature1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feature1)
+    }
+}
